629829ad7d147a5207f37c02109800e86cf586e2
Olim4ik <64145001+Olim4ik@users.noreply.github.com>
2022-01-23 15:56:51 +0500

    Integrate sb admin 2 in project

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace backend\assets;
+
+use yii\bootstrap4\BootstrapPluginAsset;
+use yii\web\AssetBundle;
+use yii\bootstrap4\BootstrapAsset;
+use yii\web\JqueryAsset;
+use yii\web\YiiAsset;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'vendor/fontawesome-free/css/all.min.css',
+		'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+	    'css/sb-admin-2.min.css',
+
+    ];
+    public $js = [
+//		<!-- Bootstrap core JavaScript-->
+//	    "vendor/jquery/jquery.min.js",
+//	    "vendor/bootstrap/js/bootstrap.bundle.min.js",
+//	    <!-- Core plugin JavaScript-->
+	    "vendor/jquery-easing/jquery.easing.min.js",
+//	    <!-- Custom scripts for all pages-->
+	    "js/sb-admin-2.min.js",
+//	    <!-- Page level plugins -->
+//	    "vendor/chart.js/Chart.min.js",
+//	    <!-- Page level custom scripts -->
+//	    "js/demo/chart-area-demo.js",
+//	    "js/demo/chart-pie-demo.js",
+    ];
+    public $depends = [
+	    YiiAsset::class,
+	    JqueryAsset::class,
+	    BootstrapPluginAsset::class,
+//	    BootstrapAsset::class,
+    ];
+
+}
